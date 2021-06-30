440e05fee1aed01feb980559f7fbe61c6764daf9
Shinoby88 <86655059+Shinoby88@users.noreply.github.com>
2021-06-30 19:56:20 +0300

    Update 02_hw.py

--- Module3/home_work/02_hw.py
+++ Module3/home_work/02_hw.py
@@ -6,3 +6,9 @@ import random
 numbers = []
 # print(random.randint(10, 20))  # раскомментируйте, чтобы посмотреть работу функции randint
 # TODO: your code here
+import random
+n = int(input('введите количество случайных элементов в списке: '))
+my_list = []
+for el in range(n):
+    my_list.append(random.randint(-100, 100))
+print(my_list)
